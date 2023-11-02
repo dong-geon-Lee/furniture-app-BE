0051ab12c0b48ee337f0b25370c98051fe7cb17c
developer <ch9716@naver.com>
2023-11-02 13:59:20 +0900

    upload 모듈 구축

---
+++ src/upload/upload.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('upload')
+export class UploadController {
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    new ParseFilePipe({
+      validators: [],
+    });
+  }
+}

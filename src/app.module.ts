15fdf99079505881a5f3173893ea1b735b533782
developer <ch9716@naver.com>
2023-11-03 12:56:53 +0900

    nest-cli로 users 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UploadModule } from './upload/upload.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UploadModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    UploadModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+  ],
 })
 export class AppModule {}

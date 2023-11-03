94e009da67cf47abbdb095e24aee06d9dc6dee01
developer <ch9716@naver.com>
2023-11-03 12:49:14 +0900

    upload 컨트롤러, 서비스, 모듈 구축

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,20 +1,37 @@
 import {
   Controller,
+  Get,
   ParseFilePipe,
   Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadService } from './upload.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller('upload')
 export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
-    new ParseFilePipe({
-      validators: [],
-    });
+  async uploadFile(
+    @UploadedFile(new ParseFilePipe({ validators: [] }))
+    file: Express.Multer.File,
+  ) {
+    const uniqueKey = `${uuidv4()}-${file.originalname}`;
+    const uploadedImageUrl = await this.uploadService.upload(
+      uniqueKey,
+      file.buffer,
+    );
+
+    return { id: uniqueKey, message: uploadedImageUrl };
+  }
+
+  @Get()
+  async getImageList() {
+    const imageList = await this.uploadService.getImageList();
+    return imageList;
   }
 }

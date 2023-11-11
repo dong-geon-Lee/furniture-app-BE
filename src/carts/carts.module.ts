2ec8a045697643743bd4b48029388b293614cfc8
developer <ch9716@naver.com>
2023-11-11 20:36:56 +0900

    cart 모듈 설정 ( 유저 카트 리소스 추가)

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CartsService } from './carts.service';
 import { CartsController } from './carts.controller';
+import { UsersService } from 'src/users/users.service';
+import { Cart } from './entities/cart.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { ProductsService } from 'src/products/products.service';
+import { Product } from 'src/products/entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cart, User, Product])],
   controllers: [CartsController],
-  providers: [CartsService],
+  providers: [CartsService, UsersService, ProductsService],
 })
 export class CartsModule {}

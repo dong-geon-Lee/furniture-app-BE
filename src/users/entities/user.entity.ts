c4142e40da526069fe6206d948c49b9081360785
developer <ch9716@naver.com>
2023-11-11 15:46:17 +0900

    user 엔티티 수정. carts 와 일대다 관계 형성

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
 // import { Product } from 'src/products/entities/product.entity';
+import { Cart } from 'src/carts/entities/cart.entity';
 import { Product } from 'src/products/entities/product.entity';
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 
@@ -19,9 +20,9 @@ export class User {
   @Column({ default: false })
   isAdmin: boolean;
 
-  // @OneToMany(() => Product, (product) => product.admin)
-  // products: Product[];
-
   @OneToMany(() => Product, (product) => product.admin)
   products: Product[];
+
+  @OneToMany(() => Cart, (cart) => cart.user)
+  carts: Cart[];
 }

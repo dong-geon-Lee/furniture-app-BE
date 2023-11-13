1963d75f4735c205c2a0ae1b123c486212dd0034
developer <ch9716@naver.com>
2023-11-13 17:22:21 +0900

    nestjs swagger 설치 및 API 통합

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Furniture 프로젝트')
+    .setDescription('Furniture API 목록입니다')
+    .setVersion('1.0')
+    .addTag('Furniture 애플리케이션')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.enableCors();
   await app.listen(5000);
 }

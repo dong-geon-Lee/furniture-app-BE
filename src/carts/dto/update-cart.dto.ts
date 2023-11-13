4bf9287c7ceb2bcd1e6d8a3ed2369b7cf149489f
developer <ch9716@naver.com>
2023-11-13 15:15:54 +0900

    carts Update Dto 선언

--- src/carts/dto/update-cart.dto.ts
+++ src/carts/dto/update-cart.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCartDto } from './create-cart.dto';
+import { IsNumber, Min } from 'class-validator';
 
-export class UpdateCartDto extends PartialType(CreateCartDto) {}
+export class UpdateCartDto extends PartialType(CreateCartDto) {
+  @IsNumber()
+  @Min(1)
+  quantity: number;
+}

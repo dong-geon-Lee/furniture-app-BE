5472f42d30e7b88eff462e840233a342734d6e73
developer <ch9716@naver.com>
2023-11-14 11:23:32 +0900

    Jwt 종속성 주입 및 RolesGuard 클래스 선언

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {}
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user.userId) {
+      throw new ForbiddenException('Access Denied');
+    }
+
+    return true;
+  }
+}

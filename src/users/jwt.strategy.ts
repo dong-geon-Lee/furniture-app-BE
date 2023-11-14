cf4e1bed1bad9b2673f58db4ec3e1402ff112a90
developer <ch9716@naver.com>
2023-11-14 11:21:57 +0900

    jwt 함수선언

---
+++ src/users/jwt.strategy.ts
@@ -0,0 +1,21 @@
+// jwt.strategy.ts
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    // { sub: 10, email: 'test@naver.com', iat: 1699875722, exp: 1799875722 }
+    return { userId: payload.sub, username: payload.email };
+  }
+}

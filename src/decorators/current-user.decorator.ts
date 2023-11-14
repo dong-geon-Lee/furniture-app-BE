622af4e1ff02335973e0572a4bf836da3d8a427b
developer <ch9716@naver.com>
2023-11-14 13:36:27 +0900

    유저 데코레이터 생성

---
+++ src/decorators/current-user.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { SetMetadata } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+export const CurrentUser = createParamDecorator(
+  (data: any, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    return request.user;
+  },
+);

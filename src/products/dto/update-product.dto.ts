e4ac5949b70dd6daafc03baa25c9acbee316122e
developer <ch9716@naver.com>
2023-11-14 14:40:19 +0900

    product update dto 선언

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,25 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @IsNumber()
+  @IsOptional()
+  price: number;
+
+  @IsString()
+  @IsOptional()
+  imageURL: string;
+
+  @IsString()
+  @IsOptional()
+  category: string;
+}

87ff457ccd778e032495c2df057e333b5121ad82
developer <ch9716@naver.com>
2023-11-17 23:04:43 +0900

    shippings 엔티티 정의

---
+++ src/shippings/entities/shipping.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({ name: 'shippings' })
+export class Shipping {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'user_name' })
+  userName: string;
+
+  @Column({ name: 'user_email' })
+  userEmail: string;
+
+  @Column({ name: 'user_phone' })
+  userPhone: string;
+
+  @Column()
+  address: string;
+
+  @Column({ default: '카카오페이' })
+  method: string;
+
+  @Column({ name: 'post_code' })
+  postcode: number;
+
+  @Column({ name: 'product_price' })
+  productPrice: number;
+
+  @Column({ name: 'product_ship' })
+  productShip: number;
+
+  @Column({ name: 'product_total' })
+  productTotal: number;
+}

83796a100b8f7e7eea62f8d7d5a02ba9fbcf75d9
developer <ch9716@naver.com>
2023-11-18 21:33:59 +0900

    shippings me API 설계 : token을 가진 유저는 본인의 정보를 가짐

--- src/shippings/shippings.controller.ts
+++ src/shippings/shippings.controller.ts
@@ -6,10 +6,13 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { ShippingsService } from './shippings.service';
 import { CreateShippingDto } from './dto/create-shipping.dto';
 import { UpdateShippingDto } from './dto/update-shipping.dto';
+import { JwtAuthGuard, RolesGuard } from 'src/guards/auth.guard';
 
 @Controller('shippings')
 export class ShippingsController {
@@ -20,14 +23,15 @@ export class ShippingsController {
     return this.shippingsService.create(body);
   }
 
+  @Get('me')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  find(@Request() req) {
+    return this.shippingsService.findOne(req.user.userId);
+  }
+
   @Get()
   findAll() {
     return this.shippingsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.shippingsService.findOne(+id);
   }
 
   @Patch(':id')

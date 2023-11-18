e6db47a253b8200588d044cdafde9f4487dd97a4
developer <ch9716@naver.com>
2023-11-18 21:36:56 +0900

    user 종속성 주입 및 관계형 서비스 로직 설계

--- src/shippings/shippings.service.ts
+++ src/shippings/shippings.service.ts
@@ -1,15 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateShippingDto } from './dto/create-shipping.dto';
 import { UpdateShippingDto } from './dto/update-shipping.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Shipping } from './entities/shipping.entity';
 import { Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class ShippingsService {
-  constructor(@InjectRepository(Shipping) private repo: Repository<Shipping>) {}
+  constructor(
+    @InjectRepository(Shipping)
+    private repo: Repository<Shipping>,
 
-  create(body: CreateShippingDto) {
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+  ) {}
+
+  async create(body: CreateShippingDto) {
+    const user = await this.userRepo.findOne({ where: { id: body.userId } });
+
     const shippings = this.repo.create({
       userName: body.userName,
       userEmail: body.userEmail,
@@ -20,24 +29,30 @@ export class ShippingsService {
       productPrice: body.productPrice,
       productShip: body.productShip,
       productTotal: body.productTotal,
+      user,
     });
 
     return this.repo.save(shippings);
   }
 
   findAll() {
-    return this.repo.find();
+    return this.repo.find({ relations: ['user'] });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} shipping`;
+    return this.repo.find({
+      where: { user: { id } },
+      relations: ['user'],
+    });
   }
 
-  update(id: number, updateShippingDto: UpdateShippingDto) {
-    return `This action updates a #${id} shipping`;
+  update(id: number, body: UpdateShippingDto) {
+    return `This action updates a #${id} shipping ${body}`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} shipping`;
+  async remove(id: number) {
+    const shippings = await this.repo.findOne({ where: { id } });
+    if (!shippings) throw new NotFoundException(`존재하지않는 id:${id}`);
+    return this.repo.remove(shippings);
   }
 }

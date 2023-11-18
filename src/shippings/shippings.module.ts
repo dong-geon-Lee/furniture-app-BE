9d8fc5de1251837056e6e2a110227035e4e1ce8c
developer <ch9716@naver.com>
2023-11-18 21:32:51 +0900

    shippings 모듈에 user 모듈 및 서비스 추가

--- src/shippings/shippings.module.ts
+++ src/shippings/shippings.module.ts
@@ -3,10 +3,12 @@ import { ShippingsService } from './shippings.service';
 import { ShippingsController } from './shippings.controller';
 import { Shipping } from './entities/shipping.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Shipping])],
+  imports: [TypeOrmModule.forFeature([Shipping, User])],
   controllers: [ShippingsController],
-  providers: [ShippingsService],
+  providers: [ShippingsService, UsersService],
 })
 export class ShippingsModule {}
